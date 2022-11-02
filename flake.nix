50629e536f4cb4037ebef9421611daa4cdb95909
janfriderici <jan@friderici.net>
2022-11-02 20:02:17 +0100

    copy pasted nix file.

--- flake.nix
+++ flake.nix
@@ -1,11 +1,36 @@
 {
   description = "A very basic flake";
-
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
+      flake-utils.lib.eachDefaultSystem (system:
+        let
+          overlays = [ (import rust-overlay) ];
+          pkgs = import nixpkgs { inherit system overlays; };
+          rustVersion = pkgs.rust-bin.stable.latest.default;
+
+          rustPlatform = pkgs.makeRustPlatform {
+            cargo = rustVersion;
+            rustc = rustVersion;
+          };
+
+          myRustBuild = rustPlatform.buildRustPackage {
+            pname =
+              "heosd"; # make this what ever your cargo.toml package.name is
+            version = "0.1.0";
+            src = ./.; # the folder with the cargo.toml
+
+            cargoLock.lockFile = ./Cargo.lock;
+          };
+
+        in {
+          defaultPackage = myRustBuild;
+          devShell = pkgs.mkShell {
+            buildInputs =
+              [ (rustVersion.override { extensions = [ "rust-src" ]; }) ];
+          };
+        });
 }
